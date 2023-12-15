40412907b94da4890051377b4277d19b38683294
kushyzee <niklaus.oa@gmail.com>
2023-12-15 23:25:48 +0100

    updated server

--- server.py
+++ server.py
@@ -27,8 +27,8 @@ def get_weather():
         "weather.html",
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
-        temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+        temp=f"{round(weather_data['main']['temp'])}",
+        feels_like=f"{round(weather_data['main']['feels_like'])}"
     )
 
 if __name__ == "__main__":
